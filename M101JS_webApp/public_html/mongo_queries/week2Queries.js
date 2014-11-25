1e68f7ca3704e3cbd5681b1317df79fbf061fb67
juliocnsouza <juliocnsouzadev@gmail.com>
2014-11-25 19:47:14 -0200

    findOne

--- M101JS_webApp/public_html/mongo_queries/week2Queries.js
+++ M101JS_webApp/public_html/mongo_queries/week2Queries.js
@@ -1,8 +1,14 @@
 //Insert:
-db.fruit.insert({'name':'apple', 'color':'red', 'shape':'round'})
+db.fruit.insert({'name': 'apple', 'color': 'red', 'shape': 'round'})
 
 //Find all:
 db.fruit.find().pretty()
 
 //Find one:
 db.fruit.findOne()
+
+//Find one with (first argmument: where) (second argument: what to show)
+db.fruit.findOne({'name': 'apple'}, {'name': true, '_id': false})
+
+//Use findOne on the collection users to find one document where the key username is "dwight", and retrieve only the key named email.
+db.users.findOne({'username': 'dwight'}, {'email': true, '_id': false})
