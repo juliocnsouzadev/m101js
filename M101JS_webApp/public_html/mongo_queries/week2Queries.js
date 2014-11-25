c89672b8a31b96324767b0b54bf168823aa1a046
juliocnsouza <juliocnsouzadev@gmail.com>
2014-11-25 20:08:14 -0200

    greater than and less than with strings

--- M101JS_webApp/public_html/mongo_queries/week2Queries.js
+++ M101JS_webApp/public_html/mongo_queries/week2Queries.js
@@ -21,3 +21,8 @@ db.scores.find({score: {$gt: 95, $lt: 98}})
 
 //finds documents with a score between 50 and 60, inclusive
 db.scores.find({score: {$gte: 50, $lte: 60}});
+
+//Which of the following will find all users with name between "F" and "Q" (Inclusive)?
+db.users.find({name: {$gte: "F", $lte: "Q"}});
+//or
+db.users.find({name: {$gte: "F", $lte: "Q"}});
