479af0bb19d62c84ff1b2e91b27f3dd89b539d28
juliocnsouza <juliocnsouzadev@gmail.com>
2014-11-25 19:55:41 -0200

    find() with args

--- M101JS_webApp/public_html/mongo_queries/week2Queries.js
+++ M101JS_webApp/public_html/mongo_queries/week2Queries.js
@@ -12,3 +12,6 @@ db.fruit.findOne({'name': 'apple'}, {'name': true, '_id': false})
 
 //Use findOne on the collection users to find one document where the key username is "dwight", and retrieve only the key named email.
 db.users.findOne({'username': 'dwight'}, {'email': true, '_id': false})
+
+//Supposing a scores collection similar to the one presented, how would you find all documents with type: essay and score: 50 and only retrieve the student field?
+db.scores.find({'type': 'essay', 'score': 50}, {'_id': false, 'student': true})
