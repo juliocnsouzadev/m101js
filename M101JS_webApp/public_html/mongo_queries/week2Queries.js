3ef45a907bc14149c8853109a33c3b53b9ad44cb
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-01 06:58:29 -0200

    little corretions

--- M101JS_webApp/public_html/mongo_queries/week2Queries.js
+++ M101JS_webApp/public_html/mongo_queries/week2Queries.js
@@ -1,13 +1,13 @@
-//Insert:
+//Insert():
 db.fruit.insert({'name': 'apple', 'color': 'red', 'shape': 'round'})
 
-//Find all:
+//Find():
 db.fruit.find().pretty()
 
-//Find one:
+//FindOne():
 db.fruit.findOne()
 
-//Find one with (first argmument: where) (second argument: what to show)
+//FindOne with (first argmument: where) (second argument: what to show)
 db.fruit.findOne({'name': 'apple'}, {'name': true, '_id': false})
 
 //Use findOne on the collection users to find one document where the key username is "dwight", and retrieve only the key named email.
