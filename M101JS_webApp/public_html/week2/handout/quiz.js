2376e7da4f56884bc38900063a13cacce7af5f81
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-06 12:22:28 -0200

    grades findOne

--- M101JS_webApp/public_html/week2/handout/quiz.js
+++ M101JS_webApp/public_html/week2/handout/quiz.js
@@ -2,8 +2,9 @@
 //execute after import the grandes.json
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/course', function (err, db)
+MongoClient.connect('mongodb://localhost:27017/m101js', function (err, db)
 {
+    console.log("conectou...");
     if (err)
         throw err;
 
@@ -12,7 +13,6 @@ MongoClient.connect('mongodb://localhost:27017/course', function (err, db)
     function callback(err, doc) {
         if (err)
             throw err;
-
         console.dir(doc);
 
         db.close();
